287ce87fc1cc478cdef2bb9272b99fbb6ed2c220
Kyle Linhardt <kylelinhardt@Kyles-iMac.local>
2017-08-05 20:03:27 -0400

    added route to get model id using the uid from sketch fab

--- server.js
+++ server.js
@@ -616,7 +616,15 @@ console.log(db);
     var modelid = req.params.modelid;
 
     db.one('SELECT * FROM models where id = $1', [modelid])
-      .then(respondWithData(res, 'here id model id: ' + modelid))
+      .then(respondWithData(res, 'heres the model for id: ' + modelid))
+      .catch(catchError(next));
+  });
+
+  app.get('/api/getmodelid/:modeluid', function(req, res, next) {
+    var modeluid = req.params.modeluid;
+
+    db.one('SELECT id FROM models where model_uid = $1', [modeluid])
+      .then(respondWithData(res, 'model id for uid: ' + modeluid))
       .catch(catchError(next));
   });
 
